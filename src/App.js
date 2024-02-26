8527edce785568deb3051b634ae465dbe3213288
vijayK006 <vijaykale7052@gmail.com>
2024-02-27 00:06:38 +0530

    greeting , username and search bug fixed and updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { FaLocationArrow  } from "react-icons/fa";
+import { IoSearchCircleSharp } from "react-icons/io5";
 
 
 function App() {
@@ -15,10 +16,11 @@ function App() {
   const [country, setCountry] = useState('');
   const [cityNotFound, setCityNotFound] = useState(false);
   
-
+  const [greeting, setGreeting] = useState('');
+  const [currentTime, setCurrentTime] = useState('');
   useEffect(() => {
-
-    if (city.trim() === '') {
+    const trimmedCity = city.trim();
+    if (trimmedCity === '') {
       // If the input field is blank, reset all weather information to 0
       setCityname('');
       setWeather(0);
@@ -52,6 +54,76 @@ setCityNotFound(false);
       });
   }, [city])
 
+
+  const getuser = ()=>{
+    const uname = document.getElementById('username').value;
+    // console.log(uname)
+// document.getElementById('nameUser').innerHTML= uname;
+const userdetails = document.getElementById('userdetails');
+userdetails.classList.add('close');
+ 
+if(uname === ''){
+document.getElementById('nameUser').innerHTML= 'Guest';
+}else{
+document.getElementById('nameUser').innerHTML= uname;
+}
+  }
+
+
+
+  const findcity =()=>{
+    const cityname = document.getElementById('cityname').value;
+setCity(cityname);
+
+if(cityname === ''){
+  setCityNotFound(true);
+  console.log('City name is empty')
+}else{
+  setCityNotFound(false);
+
+}
+
+  }
+
+
+  // useEffect(() => {
+  //   const date = new Date();
+  //   const h = date.getHours();
+
+  //   if (h >= 0 && h < 12) {
+  //     setGreeting('Good Morning!');
+  //   } else if (h >= 12 && h < 17) {
+  //     setGreeting('Good Afternoon!');
+  //   } else if (h >= 17 && h < 19) {
+  //     setGreeting('Good Evening!');
+  //   } else {
+  //     setGreeting('Good Night!');
+  //   }
+  // }, []);
+
+  
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const currentDate = new Date();
+      const currentTimeString = currentDate.toLocaleTimeString();
+      setCurrentTime(currentTimeString);
+
+      const h = currentDate.getHours();
+
+      if (h >= 0 && h < 12) {
+        setGreeting('Good Morning!');
+      } else if (h >= 12 && h < 17) {
+        setGreeting('Good Afternoon!');
+      } else if (h >= 17 && h < 19) {
+        setGreeting('Good Evening!');
+      } else {
+        setGreeting('Good Night!');
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <>
 
@@ -59,13 +131,22 @@ setCityNotFound(false);
       <div className='weather_body'>
 
         <div className='weather-app '>
-          <input type="text" placeholder='Type city name '
-            onChange={(e) => setCity(e.target.value)} className='searchinput' />
+        <div className='position-relative'>
+           <input type="text" placeholder='Type city name '
+            className='searchinput'  id='cityname' />
+            <button type='button'  onClick={findcity} className='search'>
+              <IoSearchCircleSharp className='icon'/>
+            </button>
+        </div>
+         
 
           <div className='d-flex justify-content-between py-4'>
             <div className='greet'>
-              <span className='name'>Hi, Vijay S Kale</span>
-              <h5>Good Evening</h5>
+              <span className='name'>Hi, <span id="nameUser"></span> </span>
+              <h4>{greeting}</h4>
+            </div>
+            <div className='time'>
+              <h5>{currentTime}</h5>
             </div>
 
           </div>
@@ -143,6 +224,18 @@ setCityNotFound(false);
 
       </div>
 
+<div className='model-body' id='userdetails'>
+<div className='model-center'>
+<h3 className='text-center pb-md-1 pb-3'>Explore Weather Updates from All Cities Worldwide!</h3>
+<p className='text-center pb-4'>Welcome to my website, your ultimate destination for comprehensive 
+weather reports from every city of the globe. </p>
+  <input type='text' placeholder='Please Enter Your Name' id="username" className='prompt-field'/>
+<br/>
+
+<button type='button' className='btn-name'  onClick={getuser}>Send</button>
+</div>
+
+</div>
 
 
     </>
@@ -151,3 +244,6 @@ setCityNotFound(false);
 }
 
 export default App;
+
+
+
